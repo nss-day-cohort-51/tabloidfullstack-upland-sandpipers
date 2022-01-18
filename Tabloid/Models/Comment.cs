3e13cd67ddbaeeb387bed9ba7c7238647e7f862f
Colin Miller <colinraymiller@gmail.com>
2022-01-18 10:57:41 -0600

    Create Models

---
+++ Tabloid/Models/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tabloid.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public Post Post { get; set; }
+        [Required]
+        [DisplayName("Post")]
+        public int PostId { get; set; }
+        public UserProfile UserProfile { get; set; }
+        [Required]
+        [DisplayName("User Profile")]
+        public int UserProfileId { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDateTime { get; set; }
+    }
+}
---
+++ Tabloid/Models/Subscription.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tabloid.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        [Required]
+        public string SubscriberUserProfileId { get; set; }
+        [Required]
+
+        public string ProviderUserProfileId { get; set; }
+
+        [Required]
+        public DateTime BeginDateTime { get; set; }
+
+        [Required]
+        public DateTime EndDateTime { get; set; }
+
+    }
+}
