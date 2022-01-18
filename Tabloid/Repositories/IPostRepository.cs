79689b65e974df32758584a15dc15d723754eab0
Colin Miller <colinraymiller@gmail.com>
2022-01-18 13:12:47 -0600

    PostRepository Pasta

---
+++ Tabloid/Repositories/IPostRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Tabloid.Models;
+
+namespace Tabloid.Repositories
+{
+    internal interface IPostRepository
+    {
+        void Add(Post post);
+        void Delete(int id);
+        List<Post> GetAllPublishedPosts();
+        List<Post> GetPostsByUserId(int id);
+        Post GetPublishedPostById(int id);
+        Post GetUserPostById(int id, int userProfileId);
+        void Update(Post post);
+    }
+}
