b0137b5fef52ea28389ef278a93e29568d921957
JamaCodes <jama.avwork@gmail.com>
2022-01-18 14:39:20 -0600

    ***[all tags] --Tag repos created --tagcontroller created

---
+++ Tabloid/Controllers/TagController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid.Repositories;
+
+namespace Tabloid.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly ITagRepository _tagRepository;
+
+        public TagController(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
