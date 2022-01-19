15a07b7b541fa7af4a4e4cbbce2952071f22b2f4
Colin Miller <colinraymiller@gmail.com>
2022-01-19 13:31:15 -0600

    update tag edit/delete links

--- Tabloid/client/src/components/Tag.js
+++ Tabloid/client/src/components/Tag.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { Button } from 'reactstrap';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Button } from "reactstrap";
 
 const Tag = ({ tag }) => {
-  return (
-    <tr>
-      <th scope="row">{tag.name}</th>
-      <td>
-        <Button color="primary">edit</Button>{' '}
-        <Button color="danger">delete</Button>{' '}
-      </td>
+    const history = useHistory();
 
-    </tr>
-  );
-}
+    const handleDelete = () => {
+        history.push(`/delete/${tag.id}`);
+    };
+    const handleEdit = () => {
+        history.push(`/edit/${tag.id}`);
+    };
+
+    return (
+        <tr>
+            <th scope="row">{tag.name}</th>
+            <td>
+                <Button color="primary" onClick={handleDelete}>
+                    edit
+                </Button>
+                <Button color="primary" onClick={handleEdit}>
+                    delete
+                </Button>
+            </td>
+        </tr>
+    );
+};
 
 export default Tag;
