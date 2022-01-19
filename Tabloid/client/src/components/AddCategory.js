99d1658b974588364086da04fc1d016885eab6d1
JamaCodes <jama.avwork@gmail.com>
2022-01-19 11:10:40 -0600

    created AddCategory

---
+++ Tabloid/client/src/components/AddCategory.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+import { addCategory } from '../modules/CategoryManager';
+
+export const AddCategory = () => {
+
+    const [category, setCategory] = useState({
+        name: ""
+    });
+
+    const history = useHistory();
+
+
+    const handleControlledInputChange = (event) => {
+        const newCategory = { ...category }
+        let selectedVal = event.target.value
+        // forms always provide values as strings. But we want to save the ids as numbers.
+        if (event.target.id.includes("Id")) {
+            selectedVal = parseInt(selectedVal)
+        }
+        newCategory[event.target.id] = selectedVal
+        // update state
+        setCategory(newCategory)
+    }
+
+    const handleClickSaveCategory = (event) => {
+        event.preventDefault() //Prevents the browser from submitting the form
+        console.log(category);
+        addCategory(category)
+            .then(() => history.push("/tags"))
+
+    }
+
+    return (
+        <form className="main-content">
+            <h2 className="_title">New Vehicle:</h2>
+            <fieldset className="fieldset">
+                <div className="form-group">
+                    <label htmlFor="name">Category name:</label>
+                    <input type="text" id="name" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Name" value={category.name} />
+                </div>
+            </fieldset>
+            <button className="btn-add-save"
+                onClick={handleClickSaveCategory}>
+                Save Category
+            </button>
+            <button className="btn-add-edit"
+                onClick={() => history.push("/tags")}>
+                Cancel
+            </button>
+        </form>
+    )
+};
