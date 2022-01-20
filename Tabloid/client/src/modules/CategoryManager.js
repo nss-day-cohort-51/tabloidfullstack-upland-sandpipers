40bc32a9e7cb1646b9c40ac84ebeea6bdf721dd0
Colin Miller <colinraymiller@gmail.com>
2022-01-20 13:24:23 -0600

    update CategoryManager

--- Tabloid/client/src/modules/CategoryManager.js
+++ Tabloid/client/src/modules/CategoryManager.js
@@ -1,58 +1,89 @@
+import { getToken } from "./authManager";
+
 const baseUrl = "/api/category";
 
 export const getAllCategories = () => {
-    return fetch(baseUrl).then((res) => res.json());
+    return getToken().then((token) => {
+        return fetch(baseUrl, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }).then((res) => res.json());
+    });
 };
 
 export const addCategory = (category) => {
-    return fetch(baseUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(category),
+    return getToken().then((token) => {
+        return fetch(baseUrl, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+
+            body: JSON.stringify(category),
+        });
     });
 };
 
 export const getWithComments = () => {
-    return fetch(baseUrl + "/getwithcomments", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }).then((res) => res.json());
+    return getToken().then((token) => {
+        return fetch(baseUrl + "/getwithcomments", {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+        }).then((res) => res.json());
+    });
 };
 
 export const getBySearch = (q, isSort) => {
-    return fetch(baseUrl + `/search?q=${q}&sortDesc=${isSort}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }).then((res) => res.json());
+    return getToken().then((token) => {
+        return fetch(baseUrl + `/search?q=${q}&sortDesc=${isSort}`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+        }).then((res) => res.json());
+    });
 };
 
 export const getCategoryById = (categoryId) => {
-    return fetch(baseUrl + `/${categoryId}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }).then((resp) => resp.json());
+    return getToken().then((token) => {
+        return fetch(baseUrl + `/${categoryId}`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+        }).then((resp) => resp.json());
+    });
 };
 
 export const deleteCategory = (categoryId) => {
-    return fetch(baseUrl + `/${categoryId}`, {
-        method: "DELETE"
-    })
-}
+    return getToken().then((token) => {
+        return fetch(baseUrl + `/${categoryId}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+    });
+};
 
 export const updateCategory = (category) => {
-    return fetch(`${baseUrl}/${category.id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(category),
+    return getToken().then((token) => {
+        return fetch(`${baseUrl}/${category.id}`, {
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+
+            body: JSON.stringify(category),
+        });
     });
 };
